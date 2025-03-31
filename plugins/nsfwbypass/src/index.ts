dec44e968a7782612a14214ad81d679f6794716e
dotle31 <abacubabacus@gmail.com>
2025-03-31 23:52:30 +1100

    meow

--- plugins/nsfwbypass/src/index.ts
+++ plugins/nsfwbypass/src/index.ts
@@ -1,14 +1,9 @@
 import { findByStoreName, findByProps } from "@vendetta/metro"
 import { after, instead } from "@vendetta/patcher"
 
-const NSFWStuff = findByProps("isNSFWInvite")
 const UserStore = findByStoreName("UserStore")
 
 let patches = []
-
-patches.push(instead("handleNSFWGuildInvite", NSFWStuff, () => false))
-patches.push(instead("isNSFWInvite", NSFWStuff, () => false))
-patches.push(instead("shouldNSFWGateGuild", NSFWStuff, () => false))
 
 patches.push(after("getCurrentUser", UserStore, (_, user) => {
     if (user?.hasOwnProperty("nsfwAllowed")) {
