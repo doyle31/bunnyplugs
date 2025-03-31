e5a29236d7748460a5a087ecd64088d205707a26
dotle31 <abacubabacus@gmail.com>
2025-03-31 23:29:29 +1100

    meow i hope this works

---
+++ plugins/template/nsfwbypass/index.ts
@@ -0,0 +1,25 @@
+import { findByStoreName, findByProps } from "@vendetta/metro"
+import { after, instead } from "@vendetta/patcher"
+
+const NSFWStuff = findByProps("isNSFWInvite")
+const UserStore = findByStoreName("UserStore")
+
+let patches = []
+
+patches.push(instead("handleNSFWGuildInvite", NSFWStuff, () => false))
+patches.push(instead("isNSFWInvite", NSFWStuff, () => false))
+patches.push(instead("shouldNSFWGateGuild", NSFWStuff, () => false))
+
+patches.push(after("getCurrentUser", UserStore, (_, user) => {
+    if (user?.hasOwnProperty("nsfwAllowed")) {
+        user.nsfwAllowed = true
+    }
+    if (user?.hasOwnProperty("ageVerificationStatus")) {
+        user.ageVerificationStatus = 3
+    }
+    return user
+}))
+
+export const onUnload = () => {
+    for (const unpatch of patches) unpatch()
+}
